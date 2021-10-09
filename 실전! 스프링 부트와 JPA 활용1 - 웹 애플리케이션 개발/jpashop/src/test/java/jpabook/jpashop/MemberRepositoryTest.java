6341641b814d04bbd56e74c26b684b4061b7b201
alisyabob <sjs939883@gmail.com>
2021-10-09 15:07:38 +0900

    엔티티 클래스 개발1

--- 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,7 +1,5 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.Member;
-import jpabook.jpashop.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-import javax.persistence.EntityManager;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
