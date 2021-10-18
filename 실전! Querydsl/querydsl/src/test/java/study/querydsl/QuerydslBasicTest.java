075f8c827b540481dc1cb98fd360923e325f313c
alisyabob <sjs939883@gmail.com>
2021-10-18 10:39:04 +0900

    Q클래스 인스턴스 사용

--- 실전! Querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ 실전! Querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -24,6 +24,8 @@ public class QuerydslBasicTest {
 
     JPAQueryFactory queryFactory;
 
+    QMember member = QMember.member;
+
     @BeforeEach
     public void before(){
         queryFactory = new JPAQueryFactory(em);
@@ -145,8 +147,8 @@ public class QuerydslBasicTest {
     @Test
     public void paging1(){
         List<Member> result = queryFactory
-                .selectFrom(QMember.member)
-                .orderBy(QMember.member.username.desc())
+                .selectFrom(member)
+                .orderBy(member.username.desc())
                 .offset(1)
                 .limit(2)
                 .fetch();
@@ -156,8 +158,8 @@ public class QuerydslBasicTest {
     @Test
     public void paging2(){
         QueryResults<Member> queryResults = queryFactory
-                .selectFrom(QMember.member)
-                .orderBy(QMember.member.username.desc())
+                .selectFrom(member)
+                .orderBy(member.username.desc())
                 .offset(1)
                 .limit(2)
                 .fetchResults();
