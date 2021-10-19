cd2db5a7ec8e477b67d71dfb26ee619b8669781c
alisyabob <sjs939883@gmail.com>
2021-10-19 11:15:06 +0900

    상수, 문자 더하기

--- 실전! Querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ 실전! Querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -429,5 +429,18 @@ public class QuerydslBasicTest {
                 .fetch();
     }
 
+    @Test
+    public void concat(){
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+
+        for (String s:result){
+            System.out.println(" s="+s);
+        }
+    }
+
 
 }
