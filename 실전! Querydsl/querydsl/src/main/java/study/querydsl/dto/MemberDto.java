538eaff3ba78ce85e71782d1567543c31de5a007
alisyabob <sjs939883@gmail.com>
2021-10-20 11:15:46 +0900

    프로젝션과 결과반환 @QueryProjection

--- 실전! Querydsl/querydsl/src/main/java/study/querydsl/dto/MemberDto.java
+++ 실전! Querydsl/querydsl/src/main/java/study/querydsl/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package study.querydsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,8 +11,13 @@ public class MemberDto {
     private String username;
     private int age;
 
+    @QueryProjection
     public MemberDto(String username, int age) {
         this.username = username;
         this.age = age;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
