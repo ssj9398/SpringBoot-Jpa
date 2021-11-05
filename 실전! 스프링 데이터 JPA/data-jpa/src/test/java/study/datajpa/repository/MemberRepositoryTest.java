27cdfb30edd13aa07fff88e30095da98ed3c82b0
alisyabob <sjs939883@gmail.com>
2021-11-05 11:39:00 +0900

    @Query, 리포지토리 메소드에 쿼리 정의하기

--- 실전! 스프링 데이터 JPA/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 실전! 스프링 데이터 JPA/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -97,4 +97,17 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getUsername()).isEqualTo("AAA");
     }
 
+    @Test
+    public void testQuery(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0).getAge()).isEqualTo(10);
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+    }
 }
