5bb2de12c8a0db2aa024cb76f9778d9acd1919bc
alisyabob <sjs939883@gmail.com>
2021-11-05 17:49:03 +0900

    스프링 데이터 JPA 페이징과 정렬(1)

--- 실전! 스프링 데이터 JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 실전! 스프링 데이터 JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,5 +1,8 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -34,4 +37,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findListByUsername(String username);
     Member findMemberByUsername(String username);
     Optional<Member> findOptionalByUsername(String username);
+
+    Slice<Member> findByAge(int age, Pageable pageable);
+
+
 }
