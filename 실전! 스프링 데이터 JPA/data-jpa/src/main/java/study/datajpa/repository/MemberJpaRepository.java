77639e9555313a65fe5c6a1134e91aeea382cebd
alisyabob <sjs939883@gmail.com>
2021-11-05 17:05:34 +0900

    순수 JPA 페이징과 정렬

--- 실전! 스프링 데이터 JPA/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ 실전! 스프링 데이터 JPA/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -14,44 +14,58 @@ public class MemberJpaRepository {
     @PersistenceContext
     private EntityManager em;
 
-    public Member save(Member member){
+    public Member save(Member member) {
         em.persist(member);
         return member;
     }
 
-    public void delete(Member member){
+    public void delete(Member member) {
         em.remove(member);
     }
 
-    public List<Member> findAll(){
-        return  em.createQuery("select m from Member m", Member.class)
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
 
-    public Optional<Member> findById(Long id){
-        Member member = em.find(Member.class,id);
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
         return Optional.ofNullable(member);
     }
 
-    public long count(){
+    public long count() {
         return em.createQuery("select count(m) from Member m", Long.class)
                 .getSingleResult();
     }
 
-    public Member find(Long id){
+    public Member find(Long id) {
         return em.find(Member.class, id);
     }
 
-    public List<Member> findByUsernameAndAgeGreaterThen(String username, int age){
+    public List<Member> findByUsernameAndAgeGreaterThen(String username, int age) {
         return em.createQuery("select m from Member m where m.username = :username and m.age > :age")
-                .setParameter("username",username)
-                .setParameter("age",age)
+                .setParameter("username", username)
+                .setParameter("age", age)
                 .getResultList();
     }
 
-    public List<Member> findByUsername(String username){
-        return em.createNamedQuery("Member.findByUsername",Member.class)
-                .setParameter("username",username)
+    public List<Member> findByUsername(String username) {
+        return em.createNamedQuery("Member.findByUsername", Member.class)
+                .setParameter("username", username)
                 .getResultList();
+    }
+
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age){
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
     }
 }
