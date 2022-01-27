8215de30236dfababb51123e4f53ce4728efcb60
alisyabob <sjs939883@gmail.com>
2022-01-28 01:47:31 +0900

    [ADD] 플러시

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,17 +17,21 @@ public class JpaMain {
         tx.begin();
 
         try {
-//          //비영속
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L,"B");
-
-            em.persist(member1);
-            em.persist(member2);
-            System.out.println("====================");
+////          //비영속
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L,"B");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+//            System.out.println("====================");
 //            member.setId(101L);
 //            member.setName("HelloJPA");
 //
 //            //영속
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+            em.flush();
+            System.out.println("=================");
 //            System.out.println("=== BEFORE ===");
 //            em.persist(member);
 //            System.out.println("=== AFTER ===");
